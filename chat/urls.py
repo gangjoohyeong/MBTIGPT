54f5be71f248a97f49787027ef6ab31a7714d8e1
gangjoohyeng <bles@kakao.com>
2023-07-31 18:37:31 +0900

    feat: url 별칭

--- chat/urls.py
+++ chat/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name = 'chat'
+
 urlpatterns = [
-    path('', views.index),
-    path('<int:chatroom_id>/', views.detail),
+    path('', views.index, name='index'),
+    path('<int:chatroom_id>/', views.detail, name='detail'),
 ]
