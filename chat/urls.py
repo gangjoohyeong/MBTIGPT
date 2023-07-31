d4f8a5be756fd7f99e077244bf679d3a9cba1f85
gangjoohyeng <bles@kakao.com>
2023-07-31 18:33:58 +0900

    feat: views and templates

--- chat/urls.py
+++ chat/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
+    path('<int:chatroom_id>/', views.detail),
 ]
--- chat/views.py
+++ chat/views.py
@@ -1,4 +1,12 @@
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from .models import Chatroom
 
 def index(request):
-    return HttpResponse("이곳은 채팅 메인 페이지입니다.")
+    chatroom_list = Chatroom.objects.order_by('-create_date') # '-': 역방향
+    context = {'chatroom_list': chatroom_list}
+    return render(request, 'chat/chatroom_list.html', context)
+
+def detail(request, chatroom_id):
+    chatroom = get_object_or_404(Chatroom, pk=chatroom_id)
+    context = {'chatroom': chatroom}
+    return render(request, 'chat/chatroom_detail.html', context)
