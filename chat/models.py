b0b50391db950aca36bf493b127941534076e9da
gangjoohyeng <bles@kakao.com>
2023-07-31 15:59:32 +0900

    feat: models

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Chatroom(models.Model):
+    title = models.TextField()
+    create_date = models.DateTimeField()
+    
+    def __str__(self):
+        return self.title
+
+class Qna(models.Model):
+    chatroom = models.ForeignKey(Chatroom, on_delete=models.CASCADE)
+    question = models.TextField()
+    answer = models.TextField()
+    create_date = models.DateTimeField()
+
+
+
+
