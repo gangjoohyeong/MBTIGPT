f6552d27b593887598b8e961b739e608c7c491e4
gangjoohyeng <bles@kakao.com>
2023-07-31 18:55:48 +0900

    feat: save data

--- chat/urls.py
+++ chat/urls.py
@@ -7,4 +7,5 @@ app_name = 'chat'
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:chatroom_id>/', views.detail, name='detail'),
+    path('qna/create/<int:chatroom_id>', views.qna_create, name='qna_create')
 ]
--- chat/views.py
+++ chat/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Chatroom
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
+from .models import Chatroom, Qna
 
 def index(request):
     chatroom_list = Chatroom.objects.order_by('-create_date') # '-': 역방향
@@ -10,3 +11,11 @@ def detail(request, chatroom_id):
     chatroom = get_object_or_404(Chatroom, pk=chatroom_id)
     context = {'chatroom': chatroom}
     return render(request, 'chat/chatroom_detail.html', context)
+
+def qna_create(request, chatroom_id):
+    chatroom = get_object_or_404(Chatroom, pk=chatroom_id)
+    # chatroom.qna_set.create(question=request.POST.get('question'), answer='더미 answer', create_date=timezone.now())
+    qna = Qna(chatroom=chatroom, question=request.POST.get('question'), answer='더미 answer', create_date=timezone.now())
+    qna.save()
+    
+    return redirect('chat:detail', chatroom_id=chatroom.id)
