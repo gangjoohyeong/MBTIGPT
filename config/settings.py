3ad733f6dd238d238cdbe83a08b3c6190e42508e
gangjoohyeng <bles@kakao.com>
2023-08-01 09:56:48 +0900

    fix: type error (,)

--- config/settings.py
+++ config/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'common.apps.CommonConfig',
     'chat.apps.ChatConfig',
     'django.contrib.admin',
     'django.contrib.auth',
