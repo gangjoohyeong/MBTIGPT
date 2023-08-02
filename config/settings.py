de560bcad830435ff38e1b6e62398aff7ac4e85d
gangjoohyeng <bles@kakao.com>
2023-08-02 22:01:50 +0900

    feat: postgresql

--- config/settings.py
+++ config/settings.py
@@ -1,3 +1,6 @@
+import os
+os.environ.get("key")
+
 """
 Django settings for config project.
 
@@ -77,8 +80,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get("DB_NAME"),
+        'USER': os.environ.get("DB_USER"),
+        'PASSWORD': os.environ.get("DB_PASSWORD"),
+        'HOST': os.environ.get("DB_HOST"),
+        'PORT': os.environ.get("DB_PORT"),
     }
 }
 
